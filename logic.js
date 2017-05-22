3f614f1bf4ea85d5a484b8788b1892312345bede
JALoquacious <jalikoudis@protonmail.com>
2017-05-22 14:49:38 -0400

    Experimental quote scraping no longer works consistently; switched to using API

--- logic.js
+++ logic.js
@@ -1,47 +1,57 @@
-'use strict';
-
-var text, quotes, authors, idx, orig, url;
-
-function parseData() {
-    $.getJSON('http://whateverorigin.org/get?url=' +
-        encodeURIComponent('https://litemind.com/best-famous-quotes/') +
-        '&callback=?',
-        function (data) {
-            text = data.contents.toString();
-            quotes = text
-                .match(/\d+\.\s(.*?)(?=<)/g)
-                .join(" ")
-                .split(/\d+\. /)
-                .splice(1);
-            authors = text
-                .match(/—[^ ].+?(?=<)/g)
-                .join("")
-                .split("—");
-        }
-    );
-}
-
-function getQuote() {
-    idx = Math.floor(Math.random() * quotes.length);
-    $('#quote').html(quotes[idx]);
-    $('#author').html(authors[idx]);
-}
-
-function tweetQuote() {
-    orig = 'https://twitter.com/intent/tweet?text=' +
-        encodeURIComponent((quotes[idx] + '—' + authors[idx]));
-    url = orig.replace(/'/g, "%27").replace(/"/g, "%22");
-    window.open(url);
-}
-
-function discoverAuthor() {
-    window.open('http://google.com/search?q=' + authors[idx]);
-}
-
-$('#get-quote').click(getQuote);
-$('#tweet-quote').click(tweetQuote);
-$('#discover-author').click(discoverAuthor);
-
 $(document).ready(function () {
-    parseData();
-});
+    'use strict';
+
+    function getQuote() {
+        let data;
+        
+        $.ajax({
+            type: 'GET',
+            url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader(
+                    "X-Mashape-Authorization",
+                    "DBKOYDp4nomshucwMnoEQmQksVPYp19Fm8mjsnrWWWjfa9Jzbp");
+            },
+            success: function (result) {
+                data = $.parseJSON(JSON.stringify(result));
+
+                $('#tweet-quote').click(
+                    tweetQuote.bind(null, data.quote, data.author));
+                $('#discover-author').click(
+                    discoverAuthor.bind(null, data.author));
+
+                $('#quote').text(data.quote);
+                $('#author').text(data.author);
+                elementState(['#tweet-quote', '#discover-author'], 'disabled', false);
+            },
+            error: function () {
+                console.log('ERROR');
+            }
+        });
+        return data;
+    }
+
+    function tweetQuote(quote, author) {
+        let orig = 'https://twitter.com/intent/tweet?text=' +
+            encodeURIComponent((quote + ' -- ' + author));
+        let url = orig.replace(/'/g, "%27").replace(/"/g, "%22");
+        window.open(url);
+    }
+
+    function discoverAuthor(author) {
+        window.open('http://google.com/search?q=' + author);
+    }
+
+    function elementState(argList, attr, bool) {
+        argList.map(function (node) {
+            $(node).prop(attr, bool);
+            (bool) ? $(node).addClass(attr): $(node).removeClass(attr);
+        });
+    }
+
+    // disable secondary buttons before quote retrieved
+    elementState(['#tweet-quote', '#discover-author'], 'disabled', true);
+
+    $('#get-quote').click(getQuote);
+
+}); // END DOCUMENT
